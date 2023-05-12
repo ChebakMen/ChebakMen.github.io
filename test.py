370dccf3b7521de8580cd97586fef553f4cfc89b
ChebakMen <132047867+ChebakMen@users.noreply.github.com>
2023-05-12 15:05:46 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get('http://localhost:5000')
-prerfw4rgt4r
+
 if response.status_code == 200:
     print('Success! The server responded with:', response.text)
 else:
